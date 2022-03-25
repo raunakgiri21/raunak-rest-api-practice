3e924d493dd3bb9751505f18f3ffcdd6c838b97f
Raunak <raunakgiri21@gmail.com>
2022-03-25 12:49:15 +0530

    updated for postgres database

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+import os
 from flask import Flask
 from flask_restful import Api
 from flask_jwt import JWT
@@ -7,7 +8,9 @@ from resources.items import Item, ItemList
 from resources.store import Store, StoreList
 
 app=Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI']= 'sqlite:///data.db'
+app.config['DEBUG'] = True
+
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL', 'sqlite:///data.db')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS']=False
 app.secret_key='raunak21'
 api=Api(app)
@@ -30,4 +33,8 @@ api.add_resource(UserRegister, '/register')
 if __name__=="__main__":
     from db import db
     db.init_app(app)
+    if app.config['DEBUG']:
+        @app.before_first_request
+        def create_tables():
+            db.create_all()
     app.run(port=5000,debug=True)
