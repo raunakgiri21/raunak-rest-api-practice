985838b3389c55b24acd5701f2f1573551756dd5
Raunak <raunakgiri21@gmail.com>
2022-03-25 13:17:49 +0530

    fixed postgres uri

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 import os
+import re
 
 from flask import Flask
 from flask_restful import Api
@@ -14,6 +15,9 @@ app = Flask(__name__)
 app.config['DEBUG'] = True
 
 app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL', 'sqlite:///data.db')
+uri=app.config['SQLALCHEMY_DATABASE_URI']
+if uri.startswith("postgres://"):
+    uri = uri.replace("postgres://", "postgresql://", 1)
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.secret_key = 'jose'
 api = Api(app)
